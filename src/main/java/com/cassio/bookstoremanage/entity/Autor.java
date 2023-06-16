51697c8a996ea28412194fb2f43f647b2618456a
cassio <cassio00710@hotmail.com>
2023-06-15 22:41:59 -0300

    Criando Repositorio

--- src/main/java/com/cassio/bookstoremanage/entity/Autor.java
+++ src/main/java/com/cassio/bookstoremanage/entity/Autor.java
@@ -1,2 +1,25 @@
-package com.cassio.bookstoremanage.entity;public class Autor {
+package com.cassio.bookstoremanage.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
 }
--- src/main/java/com/cassio/bookstoremanage/entity/Book.java
+++ src/main/java/com/cassio/bookstoremanage/entity/Book.java
@@ -1,4 +1,43 @@
 package com.cassio.bookstoremanage.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private String chapters;
+
+    @Column(nullable = false)
+    private  String isbn;
+
+    @Column(name = "publisher_name", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch =FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "autor_id" )
+    private Autor autor;
+
+
 }
